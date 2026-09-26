88c80e6168e82af171fd9c1b24ce3ddd2a0a9e4c
hanzemo <ghhhh49@outlook.com>
2026-09-26 11:38:10 +0800

    fix: add InverseBoolConverter to avoid x:Bind type mismatch crash

---
+++ Converters/InverseBoolConverter.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Data;
+
+namespace DailyFortune.WinUI.Converters;
+
+public class InverseBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+        => value is bool b && !b;
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+        => value is bool b && !b;
+}
